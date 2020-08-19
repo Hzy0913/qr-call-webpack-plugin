14ded0a0fa64af4657a4009b70af812c0b6149ed
Hzy0913 <hanzhaoy@chanjet.com>
2020-08-19 16:31:46 +0800

    feat: update test webpack config

---
+++ webpack.config.js
@@ -0,0 +1,39 @@
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const QrCodeWebpackPlugin = require('./index')
+
+module.exports = {
+  mode: 'development',
+  devtool: 'source-map',
+  entry: __dirname + '/example/index.js',
+  output: {
+    path: __dirname + '/dist',
+    filename: 'index_bundle.js'
+  },
+  devServer: {
+    contentBase: path.join(__dirname, 'dist'),
+    compress: true,
+    port: 3000,
+    after: function server(app, server, compiler) {
+      app.get('/test', function (req, res) {
+        console.log('test')
+        res.send()
+      })
+    }
+  },
+  module: {
+    rules: [{
+      test: /(\.jsx|\.js)$/,
+      use: {
+        loader: 'babel-loader',
+      },
+      exclude: /node_modules/
+    }]
+  },
+  plugins: [
+    new HtmlWebpackPlugin(),
+    new QrCodeWebpackPlugin({
+      // small: true
+    }),
+  ]
+}
