76c0c3abbe0c7ac4df6549dfa02343bbb7714331
Hzy0913 <hanzhaoy@chanjet.com>
2020-08-19 16:46:05 +0800

    feat: update example

--- example/index.js
+++ example/index.js
@@ -1,2 +1,4 @@
+import './test'
+
 document.write('<h1 style="text-align: center">qrcode-webpack-plugin</h1>' +
   '<p style="text-align: center"> 🐳 You can input <span style="color: crimson">qr()</span> of the console, and show qrcode of your terminal.<\/p>')
